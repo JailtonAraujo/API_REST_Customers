e27a1bb00ddcdd5c68d423d10f180f030cc4e1bc
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-10-05 23:34:46 -0300

    Initializer project

---
+++ API_REST_Customers/src/main/java/com/br/API_REST_Customers/ApiRestCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.br.API_REST_Customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCustomersApplication.class, args);
+	}
+
+}
---
+++ API_REST_Customers/src/test/java/com/br/API_REST_Customers/ApiRestCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.API_REST_Customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
