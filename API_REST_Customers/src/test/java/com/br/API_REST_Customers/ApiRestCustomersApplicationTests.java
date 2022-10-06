a3f0360ded4713f9d3ff3a78d8a9a9e8465180e0
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-10-06 00:03:41 -0300

    Init

--- API_REST_Customers/src/test/java/com/br/API_REST_Customers/ApiRestCustomersApplicationTests.java
+++ API_REST_Customers/src/test/java/com/br/API_REST_Customers/ApiRestCustomersApplicationTests.java
@@ -1,13 +1,11 @@
 package com.br.API_REST_Customers;
 
-import org.junit.jupiter.api.Test;
+
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiRestCustomersApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
