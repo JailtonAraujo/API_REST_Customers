0d7e5828233a5336ad9e5a62354a34af6d286e3b
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2023-02-02 00:01:01 -0300

    update

--- src/main/java/com/br/API_REST_Customers/ApiRestCustomersApplication.java
+++ src/main/java/com/br/API_REST_Customers/ApiRestCustomersApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiRestCustomersApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiRestCustomersApplication.class, args);
 	}
 
--- src/test/java/com/br/API_REST_Customers/ApiRestCustomersApplicationTests.java
+++ src/test/java/com/br/API_REST_Customers/ApiRestCustomersApplicationTests.java
@@ -2,10 +2,28 @@ package com.br.API_REST_Customers;
 
 
 
+import com.br.API_REST_Customers.document.Customer;
+import com.br.API_REST_Customers.repository.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ApiRestCustomersApplicationTests {
 
+    @Autowired
+    private CustomerRepository repository;
+
+
+    @Test
+    void findAllCustomers(){
+
+        List<Customer> customers = repository.findAll();
+
+
+
+    }
 
 }
